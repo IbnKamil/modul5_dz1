4f3aabfa905e011ca326670bfd0f94578e164313
IbnKamil <163489394+IbnKamil@users.noreply.github.com>
2024-04-27 05:47:50 +0300

    Add files via upload

---
+++ DZ_1.py
@@ -0,0 +1,6 @@
+class House():
+    numberOfFloors = 10
+    numberOfFloors_now = 0
+    while numberOfFloors_now < numberOfFloors:
+        numberOfFloors_now += 1
+        print("Текущий этаж равен ",numberOfFloors_now)
